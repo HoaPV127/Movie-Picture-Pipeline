afe3f06658995b675865078d3641a948f6270d98
HoaPV127 <hoa.pv127@gmail.com>
2024-10-11 07:39:41 +0000

    fix CORS issue

--- starter/backend/__init__.py
+++ starter/backend/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from .movies import movies_api
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 app.register_blueprint(movies_api)
 
 # Start app
